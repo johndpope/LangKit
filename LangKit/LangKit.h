23fade8ce289ac1a7a7809b6edfda2f38f03d0ed
Richard Wei <xinranmsn@gmail.com>
2016-03-20 15:19:28 -0500

    Initial Commit

---
+++ LangKit/LangKit.h
@@ -0,0 +1,19 @@
+//
+//  LangKit.h
+//  LangKit
+//
+//  Created by Richard Wei on 3/20/16.
+//  Copyright © 2016 Richard Wei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LangKit.
+FOUNDATION_EXPORT double LangKitVersionNumber;
+
+//! Project version string for LangKit.
+FOUNDATION_EXPORT const unsigned char LangKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LangKit/PublicHeader.h>
+
+
