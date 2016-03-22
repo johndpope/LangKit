a55b5ba86ddfc7265977bde68a7bb0e8ed931e12
Richard Wei <xinranmsn@gmail.com>
2016-03-21 22:25:05 -0500

    Added components of HMM

--- Sources/SequenceLabeling/HiddenMarkovModel.swift
+++ Sources/SequenceLabeling/HiddenMarkovModel.swift
@@ -6,13 +6,51 @@
 //  Copyright © 2016 Richard Wei. All rights reserved.
 //
 
+public func ==(lhs: HiddenMarkovModel.Transition, rhs: HiddenMarkovModel.Transition) -> Bool {
+    return lhs.label1 == rhs.label1 && lhs.label2 == rhs.label2
+}
+
+public func ==(lhs: HiddenMarkovModel.Emission, rhs: HiddenMarkovModel.Emission) -> Bool {
+    return lhs.label == rhs.label && lhs.item == rhs.item
+}
+
 public class HiddenMarkovModel {
     
     public typealias ItemType = String
     public typealias LabelType = String
-
-    public init() {
         
+    
+    public struct Transition : Equatable, Hashable {
+        public var label1, label2: ItemType
+        
+        public init(label1: ItemType, label2: ItemType) {
+            self.label1 = label1
+            self.label2 = label2
+        }
+        
+        public var hashValue: Int {
+            return label1.hashValue & label2.hashValue
+        }
+    }
+
+    public struct Emission : Hashable {
+        public var label: ItemType
+        public var item: ItemType
+        
+        public var hashValue: Int {
+            return label.hashValue * item.hashValue
+        }
+    }
+
+    
+    private(set) var initial: [LabelType: Float]
+    private(set) var transition: [Transition: Float]
+    private(set) var emission: [Emission: Float]
+
+    public init(initial: [LabelType: Float], transition: [Transition: Float], emission: [Emission: Float]) {
+        self.initial = initial
+        self.transition = transition
+        self.emission = emission
     }
     
 }
@@ -26,9 +64,19 @@ extension HiddenMarkovModel : SequenceLabeler {
     
 }
 
+
 extension HiddenMarkovModel {
     
     public func tag(sentence: String) -> [(ItemType, LabelType)] {
+        // TODO
+        return []
+    }
+    
+}
+
+extension HiddenMarkovModel {
+    
+    public func viterbi(observation: [ItemType]) -> [(Float, LabelType)] {
         // TODO
         return []
     }
