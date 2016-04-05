4b43618c07f79dd9cbcd64898feea378535b6f6d
Richard Wei <xinranmsn@gmail.com>
2016-04-04 19:32:57 -0500

    actions -> demos

--- Sources/LangKitDemo/main.swift
+++ Sources/LangKitDemo/main.swift
@@ -2,16 +2,16 @@ import Foundation
 import CommandLine
 
 // Action table
-let actions: [String: () -> ()] =
+let demos: [String: () -> ()] =
 [
     "IBMModel1" : IBMModel1Demo.run,
-    "Trie" : TrieDemo.run,
+    "Trie"      : TrieDemo.run,
 ]
 
 let cli = CommandLine()
 
 let nameOption = StringOption(shortFlag: "n", required: true,
-                              helpMessage: "[ " + actions.keys.sorted().joined(separator: " | ") + " ]")
+                              helpMessage: "[ " + demos.keys.sorted().joined(separator: " | ") + " ]")
 
 cli.addOptions(nameOption)
 
@@ -27,10 +27,10 @@ do {
 let name = nameOption.value!
 
 // Unknown demo, print available demos
-guard let run = actions[name] else {
+guard let run = demos[name] else {
     print("Demo \"\(name)\" does not exist!")
     print("Available demos:")
-    print("  " + actions.keys.joined(separator: "\n  "))
+    print("  " + demos.keys.joined(separator: "\n  "))
     exit(EXIT_FAILURE)
 }
 
