bbb8190c0811d795e78972ea7382188f99765385
Richard Wei <xinranmsn@gmail.com>
2016-04-04 23:05:46 -0500

    updated file header

--- Sources/LangKit/LanguageModeling/NgramModel.swift
+++ Sources/LangKit/LanguageModeling/NgramModel.swift
@@ -1,7 +1,10 @@
-/**
- * Ngram.swift
- *
- */
+//
+//  NgramModel.swift
+//  LangKit
+//
+//  Created by Richard Wei on 3/20/16.
+//  Copyright © 2016 Richard Wei. All rights reserved.
+//
 
 public class NgramModel {
     
