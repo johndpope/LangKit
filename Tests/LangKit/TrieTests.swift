b9f5ae7a17fa9f6f8496fcab640440e1904e03e5
Richard Wei <xinranmsn@gmail.com>
2016-04-04 22:04:52 -0500

    added testInit

--- Tests/LangKit/TrieTests.swift
+++ Tests/LangKit/TrieTests.swift
@@ -11,16 +11,26 @@ import XCTest
 
 class TrieTests: XCTestCase {
     
+    func testInit() {
+        let trie = Trie<String>()
+        XCTAssertEqual(trie.count([]), 0)
+        XCTAssertEqual(trie.count(["I", "am", "happy"]), 0)
+    }
+    
     func testInsert() {
         let trie = Trie<String>()
         
         let trigram1 = ["I", "am", "happy"]
+        
         // Insert
         let trie1 = trie.insert(trigram1)
         XCTAssertEqual(trie1.count(trigram1), 1)
+        
         // Increment (re-insert)
         let trie2 = trie1.insert(trigram1)
         XCTAssertEqual(trie2.count(trigram1), 2)
+        
+        
     }
     
 }
