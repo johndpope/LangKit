40dbcf2c8c28cc9717d4e97c9e90e3f7a7f1cdd8
Richard Wei <xinranmsn@gmail.com>
2016-04-22 23:08:33 -0500

    Added tests for functional tools and helper operators

--- Tests/LangKit/FunctionalTests.swift
+++ Tests/LangKit/FunctionalTests.swift
@@ -20,6 +20,15 @@ class FunctionalTests: XCTestCase {
         XCTAssertEqual(xx >>- { [$0 + 1] }, [2, 3, 4, 5, 6, 7])
     }
 
+    func testApplicativeMap() {
+        let operations = [ curry(+)(5),
+                           curry(+)(6),
+                           curry(+)(7) ]
+        XCTAssertEqual(operations <*> [1, 2, 3], [6, 7, 8,
+                                                  7, 8, 9,
+                                                  8, 9, 10])
+    }
+
     func testMap() {
         let x: Int? = 3
         XCTAssertEqual({ $0 + 3 } <^> x, 6)
@@ -37,6 +46,20 @@ class FunctionalTests: XCTestCase {
     func testUncurry() {
         let add = uncurry({ (x: Int) in { (y: Int) in x + y }})
         XCTAssertEqual(add(1, 2), 3)
+    }
+
+    func testCompose() {
+        // Unary • Unary
+        let times10AndPlus5 = curry(+)(5) • curry(*)(10)
+        XCTAssertEqual(times10AndPlus5(10), 105)
+        // Binary • Unary
+        let floatProduct: (Int, Int) -> Float = (*) • Float.init
+        XCTAssertEqual(floatProduct(3, 2), 6.000)
+    }
+
+    func testMonadCompose() {
+        let tokenizeAndLetterize: String -> [String] = ^String.tokenized >-> ^String.letterized
+        XCTAssertEqual(["hel lo", "wor ld"] >>- tokenizeAndLetterize, ["h", "e", "l", "l", "o", "w", "o", "r", "l", "d"])
     }
 
 }
--- Tests/LangKit/OperatorTests.swift
+++ Tests/LangKit/OperatorTests.swift
@@ -11,10 +11,32 @@ import XCTest
 
 class OperatorTests: XCTestCase {
 
-    func testInvocation() {
+    func testInvoke() {
         let str = "Hello world !"
-        let tokenizer = §String.tokenized
+        let tokenizer = ^String.tokenized
         XCTAssertEqual(tokenizer(str), ["Hello", "world", "!"])
+    }
+
+    func testGenerate() {
+        let xx = [1, 2, 3, 4, 5]
+        XCTAssertEqual(!!xx, xx)
+
+        let xxSequence = (1...5)
+        XCTAssertEqual(!!xxSequence, xx)
+    }
+
+    func testOptionalArithmeticAssignment() {
+        var dict = ["foo": 0, "bar": 1]
+        dict["foo"] ?+= 1
+        XCTAssertEqual(dict, ["foo": 1, "bar": 1])
+        dict["fizz"] ?+= 2
+        XCTAssertEqual(dict, ["foo": 1, "bar": 1, "fizz": 2])
+    }
+
+    func testForcedOptionalArithmeticAssignment() {
+        var dict = ["foo": 0, "bar": 1]
+        dict["foo"] !+= 1
+        XCTAssertEqual(dict, ["foo": 1, "bar": 1])
     }
 
 }
