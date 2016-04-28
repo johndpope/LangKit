47269d0ee60d6ceca5a59522e708afb2bf1e9838
Richard Wei <xinranmsn@gmail.com>
2016-04-27 23:31:33 -0500

    Added `LineReader` and refactored `CorpusReader`

--- Sources/LangKit/FileIO/CorpusReader.swift
+++ Sources/LangKit/FileIO/CorpusReader.swift
@@ -12,21 +12,11 @@ public class CorpusReader<Item> {
 
     public typealias Sentence = [Item]
 
-    // Chunk size constant
-    private let chunkSize = 4096
-
-    private let path: String
-    private let encoding: NSStringEncoding
-    private let sentenceSeparator: String
-    private let fileHandle: NSFileHandle
-    private let buffer: NSMutableData
-    private let delimiterData: NSData
-
     // Tokenization function
     private var tokenize: String -> [Item]
 
-    // EOF state
-    private var eof: Bool = false
+    // Line reader
+    public let reader: LineReader
 
     /**
      Initialize a CorpusReader with configurations
@@ -39,81 +29,33 @@ public class CorpusReader<Item> {
     public required init?(fromFile path: String, sentenceSeparator: String = "\n",
                           encoding: NSStringEncoding = NSUTF8StringEncoding,
                           tokenizingWith tokenize: String -> [Item]) {
-        guard let handle = NSFileHandle(forReadingAtPath: path),
-              let delimiterData = sentenceSeparator.data(using: encoding),
-              let buffer = NSMutableData(capacity: chunkSize) else {
+        guard let reader = LineReader(fromFile: path, lineSeparator: sentenceSeparator, encoding: encoding) else {
             return nil
         }
-        self.path = path
-        self.encoding = encoding
-        self.sentenceSeparator = sentenceSeparator
-        self.fileHandle = handle
-        self.buffer = buffer
-        self.delimiterData = delimiterData
+        self.reader = reader
         self.tokenize = tokenize
-    }
-
-    deinit {
-        self.close()
-    }
-
-    /**
-     Close file
-     */
-    private func close() {
-        fileHandle.closeFile()
     }
 
     /**
      Go to the beginning of the file
      */
     public func rewind() {
-        fileHandle.seek(toFileOffset: 0)
-        buffer.length = 0
-        eof = false
+        reader.rewind()
     }
 
 }
 
 extension CorpusReader : IteratorProtocol {
 
-    public typealias Element = Sentence
+    public typealias Elememnt = Sentence
 
     /**
      Next tokenized sentence
 
      - returns: Tokenized sentence
      */
-    public func next() -> Element? {
-        if eof {
-            return nil
-        }
-
-        var range = buffer.range(of: delimiterData, options: [], in: NSMakeRange(0, buffer.length))
-
-        while range.location == NSNotFound {
-            let data = fileHandle.readData(ofLength: chunkSize)
-            guard data.length > 0 else {
-                eof = true
-                return nil
-            }
-            buffer.append(data)
-            range = buffer.range(of: delimiterData, options: [], in: NSMakeRange(0, buffer.length))
-        }
-
-        let maybeLine = String(data: buffer.subdata(with: NSMakeRange(0, range.location)), encoding: encoding)
-        buffer.replaceBytes(in: NSMakeRange(0, range.location + range.length), withBytes: nil, length: 0)
-
-        guard let line = maybeLine else {
-            return nil
-        }
-
-        let tokens = tokenize(line)
-
-        if !tokens.isEmpty { return tokens }
-
-        // Tail call on next line
-        return next()
+    public func next() -> [Item]? {
+        return reader.next() >>- tokenize
     }
 
 }
@@ -128,7 +70,7 @@ extension CorpusReader : Sequence {
      - returns: Iterator
      */
     public func makeIterator() -> Iterator {
-        self.rewind()
+        rewind()
         return self
     }
 
