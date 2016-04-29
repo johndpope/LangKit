a284b88c2f4f860712447b09656f863be13cd64a
Richard Wei <xinranmsn@gmail.com>
2016-04-29 14:33:33 -0500

    Kicked off ARPA support

--- Sources/LangKit/FileIO/ARPA.swift
+++ Sources/LangKit/FileIO/ARPA.swift
@@ -10,8 +10,14 @@ import Foundation
 
 public extension NgramModel {
 
-    public func writeARPA(toFile path: String) {
-
+    public func writeARPA(toFile path: String, encoding: NSStringEncoding = NSUTF8StringEncoding) {
+        guard let file = NSFileHandle(forWritingAtPath: path) else {
+            return
+        }
+        let header = "\\data\\\n"
+        file.write(header.data(using: encoding)!)
+        // FIXME!
+        file.closeFile()
     }
 
 }
