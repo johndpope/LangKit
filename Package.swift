483466e45d7a5d2f6e2c1d75610b5ecad3c12abf
Richard Wei <xinranmsn@gmail.com>
2016-05-03 23:29:48 -0500

    Updated dependency version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     exclude: ["Documentation", "Build", "Frameworks", "Examples", "LangKit-iOS"],
     dependencies:
     [
-        .Package(url: "https://github.com/xinranmsn/CommandLine", majorVersion: 2, minor: 2),
+        .Package(url: "https://github.com/xinranmsn/CommandLine", majorVersion: 2, minor: 3),
     ],
     targets:
     [
